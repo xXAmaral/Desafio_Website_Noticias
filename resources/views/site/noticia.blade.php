e25528096962a441617cc2c8cb5edb0cecadef44
Amaral <amaral.gustavo281@gmail.com>
2022-08-25 19:06:13 -0300

    website notícias

--- resources/views/site/noticia.blade.php
+++ resources/views/site/noticia.blade.php
@@ -18,7 +18,11 @@
         <p>Dos 513 deputados federais, 446 vão tentar se manter no cargo na eleição deste ano. O número equivale a 86,9% do total, superando o recorde anterior, de 1998, quando 443 tentaram se reeleger. Ao todo, 10.453 candidatos disputam uma vaga na Câmara.</p>
         <p>Na última eleição, em 2018, 404 parlamentares tentaram a reeleição. A média de candidaturas em busca de reeleição na Câmara é de 408 deputados, com base em dados de 1990 a 2022.</p>
         <hr>
-        <img class="bn-anuncio" src="imagens/anuncio.png" alt="Anúncio">
+        <div>
+          <a href="https://www.gov.br/mcom/pt-br/acesso-a-informacao/acoes-e-programas/wi-fi-brasil">
+            <img class="bn-anuncio" src="imagens/anuncio.png" alt="Anúncio">
+          </a>
+        </div>
         <hr>
         <p>De acordo com o Diap, dos outros 67 deputados em exercício, 24 vão concorrer ao Senado; 13 ao governo do estado; 5 uma vaga nas Assembleias Legislativas; 5 a vice-governador; 1 a vice-presidente; e 19 não vão concorrer a cargos públicos.</p>
         
